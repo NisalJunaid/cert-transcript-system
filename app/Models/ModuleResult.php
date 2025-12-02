778b5ec2cc74abbdaa96d6ff148e06ded5bbc4d1
NisalJunaid <105519086+NisalJunaid@users.noreply.github.com>
2025-12-02 14:29:25 +0500

    Ensure transcripts use landscape layout and computed CGPA

---
+++ app/Models/ModuleResult.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModuleResult extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'transcript_id',
+        'name',
+        'code',
+        'marks',
+        'grade',
+        'gp',
+        'cp',
+        'position',
+    ];
+
+    public function transcript()
+    {
+        return $this->belongsTo(Transcript::class);
+    }
+}
